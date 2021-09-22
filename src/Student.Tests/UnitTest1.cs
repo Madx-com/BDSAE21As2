8f34352aec781e46a9a34924e27dcc9030a5b2c4
asmt <asmt@itu.dk>
2021-09-22 18:57:57 +0200

    Implement and test ImmutableStudent

--- src/Student.Tests/UnitTest1.cs
+++ src/Student.Tests/UnitTest1.cs
@@ -39,7 +39,7 @@ namespace Student.Tests
         }
 
         [Fact]
-        public void ToSting_new_active_dropout_and_graduated_student()
+        public void ToSting_new_active_dropout_and_graduated_Student()
         {
             var newStudent = new Student(1);
             newStudent.GivenName = "Mads";
@@ -73,6 +73,34 @@ namespace Student.Tests
             Assert.Equal(activeExpected, activeStudent.ToString());
             Assert.Equal(dropouExpected, dropoutStudent.ToString());
             Assert.Equal(graduateExpected, graduateStudent.ToString());
+        }
+
+        [Fact]
+        public void ToSting_new_active_dropout_and_graduated_ImmutableStudent()
+        {
+            var newStudent = new ImmutableStudent{ ID = 1, GivenName = "Mads", Surname = "Ljungberg", StartDate = DateTime.Now.AddDays(-1) };
+            var activeStudent = new ImmutableStudent{ ID = 2, GivenName = "Paolo", Surname = "Tell", StartDate = DateTime.Now.AddDays(-30) };
+            var dropoutStudent = new ImmutableStudent{ ID = 3, GivenName = "Asmus", Surname = "Tørsleff", StartDate = DateTime.Now.AddDays(-30), EndDate = DateTime.Now.AddDays(-1) };
+            var graduateStudent = new ImmutableStudent{ ID = 4, GivenName = "Sebastian", Surname = "Fugmann", StartDate = DateTime.Now.AddDays(-60), EndDate = DateTime.Now.AddDays(-7), GraduationDate = DateTime.Now.AddDays(-1) };
+
+            var newExpected = $"Mads Ljungberg\nID: 1\nStatus: New\nEducation start: {newStudent.StartDate.ToString()}";
+            var activeExpected = $"Paolo Tell\nID: 2\nStatus: Active\nEducation start: {activeStudent.StartDate.ToString()}";
+            var dropouExpected =  $"Asmus Tørsleff\nID: 3\nStatus: Dropout\nEducation start: {dropoutStudent.StartDate.ToString()}\nEducation end: {dropoutStudent.EndDate.ToString()}";
+            var graduateExpected = $"Sebastian Fugmann\nID: 4\nStatus: Graduated\nEducation start: {graduateStudent.StartDate.ToString()}\nEducation end: {graduateStudent.EndDate.ToString()}\nGraduation day: {graduateStudent.GraduationDate.ToString()}";
+            Assert.Equal(newExpected, newStudent.ToString());
+            Assert.Equal(activeExpected, activeStudent.ToString());
+            Assert.Equal(dropouExpected, dropoutStudent.ToString());
+            Assert.Equal(graduateExpected, graduateStudent.ToString());
+        }
+
+        [Fact]
+        public void Equal_ID_1_GivenName_Rasmus_Surname_Lystrøm_StartDate_yesterday_ImmutableStudent()
+        {
+            var date = DateTime.Now.AddDays(-1);
+            var immutableRasmus1 = new ImmutableStudent{ ID = 1, GivenName = "Rasmus", Surname = "Lystrøm", StartDate = date };
+            var immutableRasmus2 = new ImmutableStudent{ ID = 1, GivenName = "Rasmus", Surname = "Lystrøm", StartDate = date };
+            
+            Assert.Equal(immutableRasmus1, immutableRasmus2);
         }
     }
 }
--- src/Student/Student.cs
+++ src/Student/Student.cs
@@ -6,6 +6,39 @@ namespace Student
 {
     public enum Status {New, Active, Dropout, Graduated}
 
+    public record ImmutableStudent{
+        public int ID {get ; init;} = Int32.MinValue; // default value should be handled differently. ie. errorhandling.
+        public string GivenName {get; init;}
+        public string Surname {get; init;}
+        public Status Status {get => evaluateAndRetrieveStatus();}
+        public DateTime StartDate {get; init;} = DateTime.Now; // StartDate should be set when created but in this not required.
+        public DateTime EndDate {get; init;}
+        public DateTime GraduationDate {get; init;}
+
+        private Status evaluateAndRetrieveStatus()
+        {
+            if (GraduationDate != DateTime.MinValue && GraduationDate < DateTime.Now)
+                return Status.Graduated;
+            else if (StartDate.AddDays(7) > DateTime.Now)
+                return Status.New;
+            else if (EndDate != DateTime.MinValue && EndDate < DateTime.Now && GraduationDate == DateTime.MinValue)
+                return Status.Dropout;
+            return Status.Active;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{GivenName} {Surname}\nID: {ID}\nStatus: {Status}");
+            if (StartDate != DateTime.MinValue)
+                sb.Append($"\nEducation start: {StartDate.ToString()}");
+            if (EndDate != DateTime.MinValue)
+                sb.Append($"\nEducation end: {EndDate.ToString()}");
+            if (GraduationDate != DateTime.MinValue)
+                sb.Append($"\nGraduation day: {GraduationDate.ToString()}");    
+            return sb.ToString();
+        }
+    }
     public class Student
     {
         public int ID {get ; init;} = Int32.MinValue; // default value should be handled differently. ie. errorhandling.
