6bc5221bd2bae601b0b3a79761e1076f640cda46
asmt <asmt@itu.dk>
2021-09-22 14:24:53 +0200

    implement and test evaluateAndRetrieveStatus

--- src/Student.Tests/UnitTest1.cs
+++ src/Student.Tests/UnitTest1.cs
@@ -6,9 +6,36 @@ namespace Student.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void EvaluateAndRetrieveStatus_is_new_active_dropout_and_graduated_for_started_yesterday_started_a_month_ago_enddate_yesterday_no_graddate_and_all_dates_set()
         {
+            var newStudent = new Student(1);
+            newStudent.GivenName = "Mads";
+            newStudent.Surname = "Ljungberg";
+            newStudent.StartDate = DateTime.Now.AddDays(-1); // Yesterday
 
+            var activeStudent = new Student(2);
+            activeStudent.GivenName = "Paolo";
+            activeStudent.Surname = "Tell";
+            activeStudent.StartDate = DateTime.Now.AddDays(-30); 
+
+            var dropoutStudent = new Student(3);
+            dropoutStudent.GivenName = "Asmus";
+            dropoutStudent.Surname = "Tørsleff";
+            dropoutStudent.StartDate = DateTime.Now.AddDays(-30); 
+            dropoutStudent.EndDate = DateTime.Now.AddDays(-1); 
+
+            
+            var graduateStudent = new Student(4);
+            graduateStudent.GivenName = "Sebastian";
+            graduateStudent.Surname = "Fugmann";
+            graduateStudent.StartDate = DateTime.Now.AddDays(-60); 
+            graduateStudent.EndDate = DateTime.Now.AddDays(-7); 
+            graduateStudent.GraduationDate = DateTime.Now.AddDays(-1); 
+
+            Assert.Equal(Status.New, newStudent.Status);
+            Assert.Equal(Status.Active, activeStudent.Status);
+            Assert.Equal(Status.Dropout, dropoutStudent.Status);
+            Assert.Equal(Status.Graduated, graduateStudent.Status);
         }
     }
 }
--- src/Student/Student.cs
+++ src/Student/Student.cs
@@ -8,10 +8,10 @@ namespace Student
 
     public class Student
     {
-        public int ID {get ; private set;} = Int32.MinValue; // default value should be handled differently. ie. errorhandling.
+        public int ID {get ; init;} = Int32.MinValue; // default value should be handled differently. ie. errorhandling.
         public string GivenName {get; set;}
         public string Surname {get; set;}
-        public Status Status {get; private set;}
+        public Status Status {get => evaluateAndRetrieveStatus();}
         public DateTime StartDate {get; set;} = DateTime.Now; // StartDate should be set when created but in this not required.
         public DateTime EndDate {get; set;}
         public DateTime GraduationDate {get; set;}
@@ -23,11 +23,17 @@ namespace Student
 
         private Status evaluateAndRetrieveStatus()
         {
+            if (GraduationDate != DateTime.MinValue && GraduationDate < DateTime.Now)
+                return Status.Graduated;
+            else if (StartDate.AddDays(7) > DateTime.Now)
+                return Status.New;
+            else if (EndDate != DateTime.MinValue && EndDate < DateTime.Now && GraduationDate == DateTime.MinValue)
+                return Status.Dropout;
+            return Status.Active;
             // if(StartDate) //== around same day. -> new
             // start set but older than new. -> active
             // EndDate set but not Grad -> dropout
             // End and Grad set -> graduated
-            return Status.New;
         }
 
         public override string ToString()
